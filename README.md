d70419d3f583b5e2d5b00e028d7232312accdbdf
Coding-Christian <christianhaney1996@gmail.com>
2024-06-21 22:39:43 -0700

    v 1.0

--- README.md
+++ README.md
@@ -1,6 +1,40 @@
-# Getting Started with Create React App
+# Mandelbrot Set Viewer
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app). You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started). To learn React, check out the [React documentation](https://reactjs.org/).
+## Math Explained
+
+The Mandelbrot Set contains complex numbers which have `sqrt(-1)` as a component. This "imaginary" component is represented by `i`. This Mandelbrot Set visualization represents these complex numbers in the XY-plane.
+
+### Set Definition
+For an imaginary number `c` and a corresponding set of imaginary numbers `z(n)` where `z(0) = 0` and `z(n+1) = z(n)^2, + c`, `c` is considered to be part of the Mandelbrot Set if `|z| <= 2` for `n ∈ [0, inf)`.
+
+### Representing Complex Numbers in 2D
+The complex numbers `c` and `z(n)` have real and imaginary components and can be represented as a sum of these two components `x + yi`. These two components are independent and their magnitudes can be mapped to points in 2D space `[x,y]`.
+
+### Squaring a Complex Number
+This can be understood using the two part representation `x + yi`:
+```
+z(n)^2
+(x + yi)^2
+(x + yi) * (x + yi)
+x^2 + 2xyi + (y^2 * i^2)
+x^2 + 2xyi + (y^2 * sqrt(-1)^2)
+x^2 + 2xyi + (y^2 * -1)
+x^2 + 2xyi + (-y^2)
+x^2 - y^2 + 2xyi
+(x^2 - y^2) + (2xy)i
+```
+
+### Calculating `z(n+1)` from `z(n)` and `c`
+After applying the squaring process above to `z(n)` just add the magnitude of each component of `c` to the corresponding component of the squared number. The reasoning is:
+```
+z(n)^2 + c
+(xz + yzi)^2 + (xc + yci)
+(xz + yzi) * (xz + yzi) + (xc + yci)
+xz^2 - yz^2 + 2xzyzi + (xc + yci)
+xz^2 - yz^2 + xc + 2xzyzi + yci
+(xz^2 - yz^2 + xc) + (2xzyz + yc)i
+```
 
 ## Available Scripts
 
@@ -38,9 +72,3 @@ If you aren’t satisfied with the build tool and configuration choices, you can
 Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
 
 You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,121 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const mandelbrotLimit = 2;
+const maxIterations = 100;
+const roundingFactor = 1000000;
+
+type real = number;
+type imaginary = number;
+type complex = [real, imaginary];
+
+function pow2(num: number): number {
+  return Math.pow(num, 2);
+};
+
+function mandelbrot(cnum: complex): number {
+  const absImaginary = (num: complex): number => Math.sqrt(pow2(num[0]) + pow2(num[1]));
+  let value: complex = [0, 0];
+  let i = 0;
+
+  while ((i < maxIterations) && (mandelbrotLimit >= absImaginary(value))) {
+    const real = pow2(value[0]) - pow2(value[1]) + cnum[0];
+    const imaginary = (2 * value[0] * value[1]) + cnum[1];
+    value = [real, imaginary];
+    i++;
+  }
+
+  const isInSet = (i === maxIterations) && (mandelbrotLimit >= absImaginary(value));
+  return isInSet ? 0 : i;
+}
+
+function round(num: number): number {
+  return Math.round(num * roundingFactor) / roundingFactor;
+}
+
 function App() {
+  const [zoom, setZoom] = React.useState(1);
+  const [center, setCenter] = React.useState([-0.5,0]);
+  const [showGrid, setShowGrid] = React.useState(false);
+
+  const width = 0.8 * Math.min(window.innerHeight, window.innerWidth);
+  const scaleFactor = 2 * mandelbrotLimit * 2 / width; //radius doubled, 2 px per point
+  const viewRadius = mandelbrotLimit / zoom;
+  const scale = scaleFactor / zoom;
+  const pan = viewRadius / 10
+  const grid = [];
+
+  for (let y = viewRadius + center[1]; y >= center[1] - viewRadius; y = round(y - scale)) {
+    const row = [];
+
+    for (let x = center[0] - viewRadius; x <= center[0] + viewRadius; x = round(x + scale)) {
+      const iterations = mandelbrot([x, y]);
+      const isInOrigin = (Math.abs(x) <= (2 * scale)) && (Math.abs(y) <= (2 * scale));
+      const isInCenter = (Math.abs(x - center[0]) <= (2 * scale)) && (Math.abs(y - center[1]) <= (2 * scale));
+      const isInSet = iterations === 0;
+      const isOnAxis = (round(Math.abs(x % 1)) <= scale) || (round(Math.abs(y % 1)) <= scale);
+      let className = "pixel";
+      let style = { backgroundColor: "white" };
+
+      if (isInOrigin && showGrid) {
+        style.backgroundColor = "red";
+      } else if (isInCenter || (isOnAxis && showGrid)) {
+        style.backgroundColor = "lightgrey";
+      }  else if (isInSet) {
+        style.backgroundColor = "black";
+      } else {
+        style.backgroundColor = `hsl(${Math.round(320 * iterations / maxIterations)}, 100%, 50%)`;
+      }
+
+      row.push(<div className={className} style={style}></div>);
+    }
+
+    grid.push(<div className="row">{row}</div>);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <title className="title">
+          <img src={logo} className="logo" alt="logo" />
+          <h1>Explore the Mandelbrot Set</h1>
+          <img src={logo} className="logo" alt="logo" />
+        </title>
+        <div className="controls">
+          <div className="coords">
+            <div>
+              <code className="label">X</code>
+              <button onClick={() => setCenter([round(center[0] - pan), center[1]])} className="button">{"<"}</button>
+              <button onClick={() => setCenter([round(center[0] + pan), center[1]])} className="button">{">"}</button>
+            </div>
+            <div>
+              <code className="label">Y</code>
+                <button onClick={() => setCenter([center[0], round(center[1] - pan)])} className="button">{"<"}</button>
+                <button onClick={() => setCenter([center[0], round(center[1] + pan)])} className="button">{">"}</button>
+            </div>
+            <div>
+              <code className="label">Z</code>
+              <button onClick={() => setZoom(0.1 * zoom)} className="button">-</button>
+              <button onClick={() => setZoom(10 * zoom)} className="button">+</button>
+            </div>
+          </div>
+          <div className="stats">
+            <p>Current Center : {center.join(" , ")}</p>
+            <p>Zoom Factor : {zoom}</p>
+            <div>
+              <button onClick={() => {
+                setZoom(1);
+                setCenter([-0.5,0]);
+              }} className="reset">Reset View</button>
+              <button onClick={() => setShowGrid(!showGrid)}>Show Grid</button>
+            </div>
+          </div>
+        </div>
       </header>
+      <>{Array.from(grid.values())}</>
+      <footer className="footer">
+        <p>Edit <code>src/App.tsx</code> and save to reload</p>
+      </footer>
     </div>
   );
 }
